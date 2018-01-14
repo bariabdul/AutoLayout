f85529aff83d27eec5a4f1f276be3093b73e8dea
bariabdul <abdul.bari.1@outlook.com>
2018-01-14 15:49:26 -0800

    built iphone interface

--- AutoLayout/AutoLayout/ViewController.swift
+++ AutoLayout/AutoLayout/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var profileLogo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        profileLogo.layer.cornerRadius = 8.0
+        profileLogo.clipsToBounds = true
     }
 
     override func didReceiveMemoryWarning() {
